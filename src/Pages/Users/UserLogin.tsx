b0412470de407f35c20c645885e8df8ea73d0bb9
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-12-01 10:52:15 -0500

    Fix login and signup pages

--- src/Pages/Users/UserLogin.tsx
+++ src/Pages/Users/UserLogin.tsx
@@ -115,90 +115,94 @@ function Login(props) {
   };
 
   return (
-    <div className="flex min-h-screen bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500  dark:from-gray-700 dark:via-gray-900 dark:to-black justify-center items-center">
-      <div className="-mt-24 max-w-7xl mx-auto w-[20rem] bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 py-8 px-6 md:px-8 rounded-xl">
-        <h3 className="font-extrabold text-4xl">Log In</h3>
-        <div className="text-center">
-          <form
-            className="grid grid-rows-4 mt-8 gap-2"
-            onSubmit={handleSubmit(submitForm)}
-          >
-            {" "}
-            <div className="bg-gray-100 py-2 rounded-lg">
-              <div className="flex justify-center">
-                <IdentificationIcon className="h-6 w-6 mr-2 mt-1" />
-                <input
-                  className={UserHandler() + "inline rounded-md border p-1"}
-                  type="text"
-                  {...register("Username")}
-                  placeholder="Username"
-                />{" "}
-              </div>
-              <p className="basis-[100%] text-red-700">
-                {" "}
-                {errors.Username?.message}{" "}
-              </p>
-            </div>{" "}
-            <div className="bg-gray-100 py-2 rounded-lg">
-              <div className="flex justify-center">
-                <AtSymbolIcon className="h-6 w-6 mr-2 mt-1" />
-                <input
-                  className={EmailHandler() + "inline rounded-md border p-1"}
-                  type="email"
-                  placeholder="Email"
-                  {...register("Email")}
-                />{" "}
-              </div>{" "}
-              <p className="text-red-700"> {errors.Email?.message} </p>
-            </div>
-            <div className="bg-gray-100 py-2 rounded-lg">
-              <div className="flex justify-center">
-                <KeyIcon className="h-6 w-6 mr-2 mt-1" />
-                <input
-                  className={PassHandler() + "inline rounded-md border p-1"}
-                  type="password"
-                  placeholder="Password"
-                  {...register("Password")}
-                />{" "}
-              </div>{" "}
-              <p className="text-red-700"> {errors.Password?.message} </p>
-            </div>
-            <div className="bg-gray-100 py-2 rounded-lg">
-              <div className="flex justify-center">
-                <KeyIcon className="h-6 w-6 mr-2 mt-1" />
-
-                <input
-                  className={ConfirmHandler() + "inline rounded-md border p-1"}
-                  type="password"
-                  placeholder="Confirm Password..."
-                  {...register("ConfirmPassword")}
-                />
-              </div>
-              {errors.ConfirmPassword && (
-                <p className="flex justify-center text-red-700">
-                  <ExclamationCircleIcon className="h-5 w-5 mr-1 mt-[0.15rem]" />
-                  Passwords Should Match!
-                </p>
-              )}
-            </div>
-            <button
-              className="font-semibold mt-2 py-2 px-3 max-w-xl mx-auto bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 rounded-md"
-              type="submit"
-              id="submit"
+    <div className="flex -mt-24 min-h-screen bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500  dark:from-gray-700 dark:via-gray-900 dark:to-black justify-center items-center">
+      <div className="flex justify-center items-center mt-24">
+        <div className="my-8 max-w-7xl mx-auto w-[20rem] bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 py-8 px-6 md:px-8 rounded-xl">
+          <h3 className="font-extrabold text-4xl">Log In</h3>
+          <div className="text-center">
+            <form
+              className="grid grid-rows-4 mt-8 gap-2"
+              onSubmit={handleSubmit(submitForm)}
             >
-              Log In
-            </button>
-            {userNotFound && (
-              <div className="flex justify-center text-red-200">
-                {" "}
-                <ExclamationCircleIcon className="h-6 w-6 mr-1" />
-                User not found.
+              {" "}
+              <div className="bg-gray-100 py-2 rounded-lg">
+                <div className="flex justify-center">
+                  <IdentificationIcon className="h-6 w-6 mr-2 mt-1" />
+                  <input
+                    className={UserHandler() + "inline rounded-md border p-1"}
+                    type="text"
+                    {...register("Username")}
+                    placeholder="Username"
+                  />{" "}
+                </div>
+                <p className="basis-[100%] text-red-700">
+                  {" "}
+                  {errors.Username?.message}{" "}
+                </p>
+              </div>{" "}
+              <div className="bg-gray-100 py-2 rounded-lg">
+                <div className="flex justify-center">
+                  <AtSymbolIcon className="h-6 w-6 mr-2 mt-1" />
+                  <input
+                    className={EmailHandler() + "inline rounded-md border p-1"}
+                    type="email"
+                    placeholder="Email"
+                    {...register("Email")}
+                  />{" "}
+                </div>{" "}
+                <p className="text-red-700"> {errors.Email?.message} </p>
               </div>
-            )}
-          </form>
-          <a href="/signup">
-            <button className="mt-2 text-white">Or Sign Up</button>
-          </a>
+              <div className="bg-gray-100 py-2 rounded-lg">
+                <div className="flex justify-center">
+                  <KeyIcon className="h-6 w-6 mr-2 mt-1" />
+                  <input
+                    className={PassHandler() + "inline rounded-md border p-1"}
+                    type="password"
+                    placeholder="Password"
+                    {...register("Password")}
+                  />{" "}
+                </div>{" "}
+                <p className="text-red-700"> {errors.Password?.message} </p>
+              </div>
+              <div className="bg-gray-100 py-2 rounded-lg">
+                <div className="flex justify-center">
+                  <KeyIcon className="h-6 w-6 mr-2 mt-1" />
+
+                  <input
+                    className={
+                      ConfirmHandler() + "inline rounded-md border p-1"
+                    }
+                    type="password"
+                    placeholder="Confirm Password..."
+                    {...register("ConfirmPassword")}
+                  />
+                </div>
+                {errors.ConfirmPassword && (
+                  <p className="flex justify-center text-red-700">
+                    <ExclamationCircleIcon className="h-5 w-5 mr-1 mt-[0.15rem]" />
+                    Passwords Should Match!
+                  </p>
+                )}
+              </div>
+              <button
+                className="font-semibold mt-2 py-2 px-3 max-w-xl mx-auto bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 rounded-md"
+                type="submit"
+                id="submit"
+              >
+                Log In
+              </button>
+              {userNotFound && (
+                <div className="flex justify-center text-red-200">
+                  {" "}
+                  <ExclamationCircleIcon className="h-6 w-6 mr-1" />
+                  User not found.
+                </div>
+              )}
+            </form>
+            <a href="/signup">
+              <button className="mt-2 text-white">Or Sign Up</button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
